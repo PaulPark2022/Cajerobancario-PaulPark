6dbae9b65241b485eb8c80ebf007bfd4843ab858
PaulPark2022 <111074148+PaulPark2022@users.noreply.github.com>
2022-09-29 15:39:51 -0500

    Update algoritmo_cajero_bancario_paul_park.py

--- algoritmo_cajero_bancario_paul_park.py
+++ algoritmo_cajero_bancario_paul_park.py
@@ -1,6 +1,7 @@
 # CajeroBancarioPaulPark
 
 saldo = 5000
+suma = 0
 
 #Definicion de la funcion de ingreso
 def monto_ingreso(suma):
@@ -11,6 +12,16 @@ def monto_ingreso(suma):
 def monto_retiro(resta):
   retiro = saldo - resta
   return retiro
+
+#Cantidad de billetes en el cajero
+dict_cajero = {
+  "billetes_1000": 1,
+  "billetes_500": 2,
+  "billetes_200": 5,
+  "billetes_100": 10,
+  "billetes_50": 20,
+  "billetes_20": 50
+}
 
 #Pantalla de inicio
 print("Bienvenido al CajeroBancarioTEC, seleccione una opcion:")
@@ -53,6 +64,34 @@ while acum < 10:
         print("d. Salir")
       else:
         retiro_total = monto_retiro(resta_input)
+        #calculo de cantidad restada al total del cajero
+        while resta_input >= 1000 and dict_cajero["billetes_1000"] >= 0:
+            resta_input = resta_input - 1000
+            dict_cajero["billetes_1000"] -= 1
+        dict_cajero["billetes_1000"] *= 1000
+        while resta_input >= 500 and dict_cajero["billetes_500"] >= 0:
+            resta_input = resta_input - 500
+            dict_cajero["billetes_500"] -= 1
+        dict_cajero["billetes_500"] *= 500
+        while resta_input >= 200 and dict_cajero["billetes_200"] >= 0:
+            resta_input = resta_input - 200
+            dict_cajero["billetes_200"] -= 1
+        dict_cajero["billetes_200"] *= 200
+        while resta_input >= 100 and dict_cajero["billetes_100"] >= 0:
+            resta_input = resta_input - 100
+            dict_cajero["billetes_100"] -= 1
+        dict_cajero["billetes_100"] *= 100
+        while resta_input >= 50 and dict_cajero["billetes_50"] >= 0:
+            resta_input = resta_input - 50
+            dict_cajero["billetes_50"] -= 1
+        dict_cajero["billetes_50"] *= 50
+        while resta_input >= 20 and dict_cajero["billetes_20"] >= 0:
+            resta_input = resta_input - 20
+            dict_cajero["billetes_20"] -= 1
+        dict_cajero["billetes_20"] *= 20
+        for billete in dict_cajero:
+          suma += dict_cajero[billete]
+        print (f"Quedan {suma} pesos en el cajero")
         print(f"Su saldo actual es {retiro_total}")
         acum += 1
         saldo = retiro_total
